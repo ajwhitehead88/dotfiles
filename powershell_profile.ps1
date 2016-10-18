06a7372f047c7f5cf2fde582a3ceb46d0ca3057c
Andrew Whitehead <ajwhitehead88@gmail.com>
2016-10-18 11:32:32 +0100

    Updated resharper and powershell

--- powershell_profile.ps1
+++ powershell_profile.ps1
@@ -27,6 +27,11 @@ Import-Module PSReadline
 Set-PSReadlineOption -EditMode Emacs
 Set-PSReadlineOption -BellStyle Visual
 
+Set-PSReadlineKeyHandler -Key Ctrl+Shift+LeftArrow -Function SelectShellBackwardWord
+Set-PSReadlineKeyHandler -Key Ctrl+Shift+RightArrow -Function SelectShellForwardWord
+Set-PSReadlineKeyHandler -Key Ctrl+LeftArrow -Function ShellBackwardWord
+Set-PSReadlineKeyHandler -Key Ctrl+RightArrow -Function ShellForwardWord
+Set-PSReadlineKeyHandler -Key Ctrl+Backspace -Function ShellBackwardKillWord
 Set-PSReadlineKeyHandler -Key UpArrow -Function HistorySearchBackward
 Set-PSReadlineKeyHandler -Key DownArrow -Function HistorySearchForward
 Set-PSReadlineKeyHandler -Key Ctrl+D -Function DeleteCharOrExit
